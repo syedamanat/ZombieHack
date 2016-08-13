c2d115e37ef9ce5a4067d2661698636c4e74eb3d
Anurag Pant <anurag.pant2014@vit.ac.in>
2016-08-14 01:15:24 +0530

    Second map commit.

--- anurag/main.lua
+++ anurag/main.lua
@@ -6,19 +6,67 @@
 
 -- Your code here
 
-local locationEnabled = true
-system.setLocationAccuracy( 10 )
+-- Create a native map view
+-- Create a native map view
+local myMap = native.newMapView( 20, 20,display.contentWidth, display.actualContentHeight)
+myMap.x = display.contentCenterX
+myMap.y = display.contentCenterY
 
-locationHandler = function( event )
-  if locationEnabled == true then
-    -- Check for error (user may have turned off location services)
-    if ( event.errorCode ) then
-        native.showAlert( "GPS Location Error", event.errorMessage, {"OK"} )
-        print( "Location error: " .. tostring( event.errorMessage ) )
-    else
-        startLat, startLon = event.latitude, event.longitude
-        print(event.latitude, event.longitude)
-    end
-  end
+local function markerListener(event)
+    print( event.type, event.markerId, event.latitude, event.longitude)
 end
-Runtime:addEventListener("location",locationHandler)
+
+local attempts = 0
+
+local locationText = display.newText( "Location: ", 0, 400, native.systemFont, 16 )
+locationText.anchorY = 0
+locationText.x = display.contentCenterX
+
+local function locationHandler( event )
+
+    local currentLocation = myMap:getUserLocation()
+
+    if ( currentLocation.errorCode or ( currentLocation.latitude == 0 and currentLocation.longitude == 0 ) ) then
+        locationText.text = currentLocation.errorMessage
+
+        attempts = attempts + 1
+
+        if ( attempts > 10 ) then
+            native.showAlert( "No GPS Signal", "Can't sync with GPS.", { "Okay" } )
+        else
+            timer.performWithDelay( 1000, locationHandler )
+        end
+    else
+        locationText.text = "Current location: " .. currentLocation.latitude .. "," .. currentLocation.longitude
+        myMap:setCenter( currentLocation.latitude, currentLocation.longitude )
+
+        local zshop =
+        {
+             title = "Shop",
+             subtitle = "",
+             listener = markerListener,
+             imageFile =  "smarker.png",
+        }
+
+        local zifst =
+        {
+             title = "Zombie Infested Area",
+             subtitle = "",
+             listener = markerListener,
+             imageFile =  "zmarker.png",
+        }
+
+        sloc={}
+        zloc={}
+
+        sloc[1]={12.9737214, 77.6164726}
+        zloc[1]={12.9736896, 77.6165566}
+
+        local shop = myMap:addMarker(sloc[1][1], sloc[1][2], zshop)
+        local ifst = myMap:addMarker(zloc[1][1], zloc[1][2], zifst)
+
+        --myMap:removeMarker(ifst)
+    end
+end
+
+locationHandler()
